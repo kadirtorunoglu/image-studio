7e4c6fc964ffb3bf7ea30f5abc5f80047609e8e7
kaan mert <kaanmertturgut@gmail.com>
2026-07-01 16:52:12 +0300

    Fixed bugs

--- filters.py
+++ filters.py
@@ -7,7 +7,7 @@ def rotate_image(input_path, output_path, angle):
     with Image.open(input_path) as image:
         rotated_image = image.rotate(angle, expand=True)
         rotated_image.save(output_path)
-        rotated_image.show()
+        
         
 def resize_image(image_path, output_path, size):
 
